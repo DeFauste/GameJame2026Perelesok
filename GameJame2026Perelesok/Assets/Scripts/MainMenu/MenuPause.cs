60c8cb1db78909637127b035a7800283ce50d76c
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2026-05-27 21:06:44 +0300

    Меню паузки набросок

---
+++ GameJame2026Perelesok/Assets/Scripts/MainMenu/MenuPause.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+    public class MenuPause : MonoBehaviour
+    {
+        [SerializeField] private Button menuGameButton;
+        [SerializeField] private Button exitGameButton;
+        
+        
+        private void OnEnable()
+        {
+            menuGameButton.onClick.AddListener(() => SceneManager.LoadScene("MainMenu", LoadSceneMode.Single));
+            exitGameButton.onClick.AddListener(() => Application.Quit());
+        }
+    }
+}
