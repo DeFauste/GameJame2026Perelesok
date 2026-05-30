90f5a17c05480f8ea3bdc586613a0f0a09b6a45e
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2026-05-30 13:57:02 +0300

    аниматор + анимации волшебника

---
+++ GameJame2026Perelesok/Assets/Scripts/Wizards/WizardFirstStage.cs
@@ -0,0 +1,7 @@
+﻿namespace Wizards
+{
+    public sealed class WizardFirstStage
+    {
+        
+    }
+}
---
+++ GameJame2026Perelesok/Assets/Scripts/Wizards/WizardsAI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Wizards
+{
+    public sealed class WizardsAI : MonoBehaviour
+    {
+        [Header("Wizard Health")] 
+        [SerializeField] private int health = 3;
+
+        public void TakeDamage(int damage)
+        {
+            damage = Mathf.Abs(damage); // Убедиться, что урон положительный
+            health -= damage;
+            HealthState();
+        }
+        
+    
+        /// <summary>
+        /// Метод для проверки состояния здоровья волшебника и выполнения действий в зависимости от текущего уровня здоровья.
+        /// </summary>
+        private void HealthState()
+        {
+            if (health == 3) // Запускаем первую стадию
+            {
+                // Действия для первой стадии (здоровье 3)
+            }
+            else if (health == 2) // Запускаем вторую стадию
+            {
+                // Действия для второй стадии (здоровье 2)
+            }
+            else if (health == 1) // Запускаем третью стадию
+            {
+                // Действия для третьей стадии (здоровье 1)
+            }
+            else if (health <= 0)
+            {
+                // Действия для смерти (здоровье 2)
+            }
+        }
+    }
+}
